78edb4d6441fbce6cb27ca96e4ab107ac38ba6c2
hjgode <hjgode@gmail.com>
2014-08-08 12:21:31 -0700

    first submit
    BattStatt8 shows a tile on start page with current battery status
    (percent charge left)

---
+++ BattStatt8/MainPage.xaml.cs
@@ -0,0 +1,234 @@
+﻿#if(DEBUG)
+#define DEBUG_AGENT
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using BattStatt8.Resources;
+
+using Microsoft.Phone.Scheduler;
+
+using Windows.Phone.Devices.Power;
+
+namespace BattStatt8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        PeriodicTask periodicTask;
+        ResourceIntensiveTask resourceIntensiveTask;
+
+        string periodicTaskName = "PeriodicAgent";
+        string resourceIntensiveTaskName = "ResourceIntensiveAgent";
+        public bool agentsAreEnabled = true;
+        bool ignoreCheckBoxEvents = false;
+
+        Battery _battery = Battery.GetDefault();
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ScheduledTaskAgent1.ScheduledAgent.updateTile();
+
+            _battery_RemainingChargePercentChanged(this, null);
+
+            _battery.RemainingChargePercentChanged += _battery_RemainingChargePercentChanged;
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        void _battery_RemainingChargePercentChanged(object sender, object e)
+        {
+            txtPercent.Text = string.Format("{0} %", _battery.RemainingChargePercent);
+            txtTime.Text = string.Format("{0} : {1}", _battery.RemainingDischargeTime.Hours, _battery.RemainingDischargeTime.Minutes);
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            ignoreCheckBoxEvents = true;
+
+            periodicTask = ScheduledActionService.Find(periodicTaskName) as PeriodicTask;
+
+            if (periodicTask != null)
+            {
+                PeriodicStackPanel.DataContext = periodicTask;
+            }
+
+            resourceIntensiveTask = ScheduledActionService.Find(resourceIntensiveTaskName) as ResourceIntensiveTask;
+            if (resourceIntensiveTask != null)
+            {
+                ResourceIntensiveStackPanel.DataContext = resourceIntensiveTask;
+            }
+
+            ignoreCheckBoxEvents = false;
+
+        }
+
+        private void PeriodicCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ignoreCheckBoxEvents)
+                return;
+            StartPeriodicAgent();
+        }
+        private void PeriodicCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (ignoreCheckBoxEvents)
+                return;
+            RemoveAgent(periodicTaskName);
+        }
+        private void ResourceIntensiveCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ignoreCheckBoxEvents)
+                return;
+            StartResourceIntensiveAgent();
+        }
+        private void ResourceIntensiveCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (ignoreCheckBoxEvents)
+                return;
+            RemoveAgent(resourceIntensiveTaskName);
+        }
+
+        private void RemoveAgent(string name)
+        {
+            try
+            {
+                ScheduledActionService.Remove(name);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void StartPeriodicAgent()
+        {
+            // Variable for tracking enabled status of background agents for this app.
+            agentsAreEnabled = true;
+
+            // Obtain a reference to the period task, if one exists
+            periodicTask = ScheduledActionService.Find(periodicTaskName) as PeriodicTask;
+
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule
+            if (periodicTask != null)
+            {
+                RemoveAgent(periodicTaskName);
+            }
+
+            periodicTask = new PeriodicTask(periodicTaskName);
+
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            periodicTask.Description = "This demonstrates a periodic task.";
+            
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(periodicTask);
+                PeriodicStackPanel.DataContext = periodicTask;
+
+                // If debugging is enabled, use LaunchForTest to launch the agent in one minute.
+#if(DEBUG_AGENT)
+    ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(60));
+#endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                    agentsAreEnabled = false;
+                    PeriodicCheckBox.IsChecked = false;
+                }
+
+                if (exception.Message.Contains("BNS Error: The maximum number of ScheduledActions of this type have already been added."))
+                {
+                    // No user action required. The system prompts the user when the hard limit of periodic tasks has been reached.
+
+                }
+                PeriodicCheckBox.IsChecked = false;
+            }
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+                PeriodicCheckBox.IsChecked = false;
+            }
+        }
+
+        private void StartResourceIntensiveAgent()
+        {
+            // Variable for tracking enabled status of background agents for this app.
+            agentsAreEnabled = true;
+
+            resourceIntensiveTask = ScheduledActionService.Find(resourceIntensiveTaskName) as ResourceIntensiveTask;
+
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule.
+            if (resourceIntensiveTask != null)
+            {
+                RemoveAgent(resourceIntensiveTaskName);
+            }
+
+            resourceIntensiveTask = new ResourceIntensiveTask(resourceIntensiveTaskName);
+
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            resourceIntensiveTask.Description = "This demonstrates a resource-intensive task.";
+
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(resourceIntensiveTask);
+                ResourceIntensiveStackPanel.DataContext = resourceIntensiveTask;
+
+                // If debugging is enabled, use LaunchForTest to launch the agent in one minute.
+#if(DEBUG_AGENT)
+                ScheduledActionService.LaunchForTest(resourceIntensiveTaskName, TimeSpan.FromSeconds(60));
+#endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                    agentsAreEnabled = false;
+
+                }
+                ResourceIntensiveCheckBox.IsChecked = false;
+            }
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+                ResourceIntensiveCheckBox.IsChecked = false;
+            }
+
+
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ ScheduledTaskAgent1/ScheduledAgent.cs
@@ -0,0 +1,166 @@
+﻿#if DEBUG
+#define DEBUG_AGENT
+#endif
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
+using System;
+
+using System.Linq;
+using Windows.Phone.Devices.Power;
+
+using System.Windows.Media.Imaging;
+
+//see http://msdn.microsoft.com/en-us/library/windows/apps/hh202941%28v=vs.105%29.aspx
+
+//see http://www.geekchamp.com/tips/wp7-working-with-images-content-vs-resource-build-action
+
+namespace ScheduledTaskAgent1
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        static ScheduledAgent()
+        {
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        public static void updateTile()
+        {
+            System.Diagnostics.Debug.WriteLine("updateTile()");
+            Battery _battery = Battery.GetDefault();
+            int iPerc = _battery.RemainingChargePercent;
+            string suffix="_000";
+            if (iPerc <= 30)
+                suffix = "_030";
+            else if (iPerc > 30 && iPerc < 50)
+                suffix = "_050";
+            else if (iPerc >= 50 && iPerc <70)
+                suffix = "_050";
+            else if (iPerc >= 70 && iPerc < 100)
+                suffix = "_070";
+            else if (iPerc == 100)
+                suffix = "_100";
+
+            IconicTileData itd = new IconicTileData();
+            itd.Count = _battery.RemainingChargePercent;
+
+            Uri uriSmall = new Uri("/images/IconicTileSmall"+suffix+".png", UriKind.Relative);
+            Uri uriMedium = new Uri("/images/IconicTileMediumLarge" + suffix + ".png", UriKind.Relative);
+            //BitmapImage imgSource = new BitmapImage(uri);
+
+            itd.IconImage = uriMedium;
+
+            itd.SmallIconImage = uriSmall;
+
+            itd.Title = string.Format("{0} %", _battery.RemainingChargePercent);
+            itd.WideContent1 = "Battery status:";
+            itd.WideContent2 = string.Format("remaining charge: {0} %", _battery.RemainingChargePercent);
+            itd.WideContent3 = string.Format("remaining time:   {0} : {1}", _battery.RemainingDischargeTime.Hours, _battery.RemainingDischargeTime.Minutes);
+
+            //the following did not work using a FlipTile, so I stay with IconTile
+            /*
+            FlipTileData ftd = new FlipTileData();
+            ftd.BackBackgroundImage = new Uri("/images/IconicTileMediumLarge" + suffix + ".png", UriKind.Relative);
+            ftd.BackContent = string.Format("Battery status {0}", _battery.RemainingChargePercent);
+            ftd.BackgroundImage = new Uri("/images/IconicTileMediumLarge" + suffix + ".png", UriKind.Relative);
+            ftd.BackTitle = string.Format("Battery: {0}%", _battery.RemainingChargePercent);
+            ftd.Count = _battery.RemainingChargePercent;
+            ftd.SmallBackgroundImage = new Uri("/images/IconicTileSmall" + suffix + ".png", UriKind.Relative);
+            ftd.Title = string.Format("Battery {0}%", _battery.RemainingChargePercent);
+            ftd.WideBackBackgroundImage = new Uri("/images/FlipCycleTileLarge.png", UriKind.Relative);
+            ftd.WideBackContent = "Battery status: " + 
+                string.Format("remaining charge: {0} %", _battery.RemainingChargePercent) + 
+                " : " + string.Format("remaining time:   {0} : {1}", _battery.RemainingDischargeTime.Hours, _battery.RemainingDischargeTime.Minutes); ;
+            ftd.WideBackgroundImage = new Uri("/images/FlipCycleTileLarge.png", UriKind.Relative);
+            */
+
+            ShellTile pTile = ShellTile.ActiveTiles.First();
+            pTile.Update(itd);
+            /*
+            try
+            {
+                if (pTile != null)
+                {
+                    if (pTile.GetType() == typeof(IconicTileData))
+                        pTile.Update(itd);
+                    else if (pTile.GetType() == typeof(FlipTileData))
+                        pTile.Update(ftd);
+                    else
+                        pTile.Update(itd);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message);
+            }
+            */
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            //TODO: Add code to perform your task in background
+            string toastMessage = "";
+
+            string battPerc = string.Format("{0} %", Battery.GetDefault().RemainingChargePercent);
+            string battTime = string.Format("{0}:{1}", Battery.GetDefault().RemainingDischargeTime.Hours, Battery.GetDefault().RemainingDischargeTime.Minutes);
+            // If your application uses both PeriodicTask and ResourceIntensiveTask
+            // you can branch your application code here. Otherwise, you don't need to.
+            if (task is PeriodicTask)
+            {
+                // Execute periodic task actions here.
+                toastMessage = "Periodic task running.";
+                
+            }
+            else
+            {
+                // Execute resource-intensive task actions here.
+                toastMessage = "Resource-intensive task running.";
+            }
+
+            toastMessage += "\n" + battPerc + "\n" + battTime;
+            
+            updateTile();
+
+            // Launch a toast to show that the agent is running.
+            // The toast will not be shown if the foreground application is running.
+            ShellToast toast = new ShellToast();
+            toast.Title = "Background Agent Sample";
+            toast.Content = toastMessage;
+            //toast.Show();
+
+            // If debugging is enabled, launch the agent again in one minute.
+#if DEBUG_AGENT
+  ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(60));
+#endif
+
+            NotifyComplete();
+        }
+    }
+}
